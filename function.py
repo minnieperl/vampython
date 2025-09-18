79d6f7fc458465af0dc88a0b61c2884ce01d11a0
minnieperl <132337612+minnieperl@users.noreply.github.com>
2025-09-18 15:48:14 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+# function can perform any task and it can reuse anytime 
+def printName():
+    print("my function name is print")
+    
+    #function calling
+    printName()
+
+    #argument function and parameter function
+    def myAge(age):
+        print("my age is",age)
+
+    myAge(33)
+

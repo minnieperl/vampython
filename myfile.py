e9339c71e3ea0654fe069953a31c30ffd7967c46
minnieperl <132337612+minnieperl@users.noreply.github.com>
2025-09-18 15:59:15 +0530

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,25 @@
+#Create file in python with name and extension
+# myFile = open("Pawan.txt", "w")
+# #write name and email in pawan text file
+# myFile.write("My name is Pawan and email is P@gmail.com")
+# #to close the file
+# myFile.close()
+
+#Read the data from file
+# readFile = open("Pawan.txt", "r")
+# print(readFile.read())
+
+#delete the file
+# import os
+# os.remove("Pawan.txt")
+
+#Create stock api in json-> java script object notation
+myStock = open("myStock.json", "w")
+# {} -> JsonObject and [] -> JsonArray
+myStock.write("{'name':'pawan sharma'}")
+
+#Create read write data in csv, excel, json, txt
+
+
+
+

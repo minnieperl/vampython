e75d6694f16612ec288e553b49e0f0c7895bd589
minnieperl <132337612+minnieperl@users.noreply.github.com>
2025-09-18 15:55:45 +0530

    Add files via upload

---
+++ pythonlibrary.py
@@ -0,0 +1,28 @@
+import numpy as np
+#assign array in numpy
+
+mydata=np.array([1,2,3,4])
+print(mydata)
+print(type(mydata))
+
+update the data in array
+
+mydata[2]=8
+print(mydata)
+
+for data in range(0,4):
+    myData[data]=a + data
+    b=b+1
+print(myData)
+
+acces the data from numpy array
+
+for data in range(0,4):
+    print(mydata[data])
+
+myFriends=np.array(["ivan","anshu","wani","anjani"])
+for name in myFriends:
+    print(name)
+
+x=3
+while x>=0

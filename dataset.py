c64866b42722ace6265dadcc3543431f701386ba
minnieperl <132337612+minnieperl@users.noreply.github.com>
2025-09-18 15:50:50 +0530

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,14 @@
+#create list, tuple, set, dict 
+#Add data pawan, manan, tanya, mukul
+#delete pawan
+#access all data
+#update mukul to mukul sharma
+mylist = ["pawan", "manan", "tanya", "mukul"]
+mylist[3] = "mukul sharma"
+print(mylist)
+
+myTuple = ("pawan", "manan", "tanya", "mukul")
+myTupleList = list(myTuple)
+myTupleList[3] = "mukul sharma"
+print(myTupleList) 
+#add manan and print the data
